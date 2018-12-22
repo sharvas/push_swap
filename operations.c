626674da8bbaaec4f19f57303e8d28d12999c2a2
Drew FINNIS <dfinnis@e2r8p1.42.fr>
2018-12-22 17:28:22 +0100

    added error and operations.c

---
+++ operations.c
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   operations.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dfinnis <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/12/22 17:08:02 by dfinnis           #+#    #+#             */
+/*   Updated: 2018/12/22 17:08:04 by dfinnis          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_sa(struct s_stack)
+{
+
+}
+
+void	ft_sb(struct s_stack)
+{
+	
+}
+
+void	ft_ss(struct s_stack)
+{
+	ft_sa();
+	ft_sb();
+}
+
+void	ft_pa(struct s_stack)
+{
+	
+}
+
+void	ft_pb(struct s_stack)
+{
+	
+}
+
+void	ft_ra(struct s_stack)
+{
+	
+}
+
+void	ft_rb(struct s_stack)
+{
+	
+}
+
+void	ft_rr(struct s_stack)
+{
+	ft_ra();
+	ft_rb();
+}
+
+void	ft_rra(struct s_stack)
+{
+	
+}
+
+void	ft_rrb(struct s_stack)
+{
+	
+}
+
+void	ft_rrr(struct s_stack)
+{
+	ft_rra();
+	ft_rrb();
+}
