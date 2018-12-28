d4b7b9462cd7f0885ee04927ccef84fc4b5f8514
Sarunas VASKELIS <svaskeli@student.42.fr>
2018-12-28 17:34:07 +0100

    p_s

--- push_swap.c
+++ push_swap.c
@@ -6,49 +6,105 @@
 /*   By: svaskeli <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/28 09:12:37 by svaskeli          #+#    #+#             */
-/*   Updated: 2018/12/28 15:28:23 by svaskeli         ###   ########.fr       */
+/*   Updated: 2018/12/28 15:53:08 by svaskeli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-char	*ft_sort(t_all *all, char *instructions)
+char	*ft_sort_less(all, tmp_a, size_a, instructions)
 {
-	t_stack *tmp_a;
-	t_stack *tmp_b;
-	int		size;
+	t_stack	*tmp_b;
+	int		count;
+	int		size_b;
 
-	tmp_a = all->a;
-	size = ft_find_len(all);
-	while (size)
+	size_b = 0;
+	while (size_a)
 	{
 		if (tmp_a->n < all->mean)
 		{
 			ft_pb(all);
-			top_b = all->b;
-			counter = 0;
-			while (all->b != top_b->prev)
+			size_b++;
+			tmp_b = all->b;
+			count = 0;
+			while (count < size_b)
 			{
-				if (top_b->n < all->b)
+				if (tmp_b->n < all->b)
 				{
-					all->b = top_b;
-					while (counter)
+					while (count)
 					{
 						ft_sb(all);
-						all->b = all->b->next;
-						counter--;
+						ft_rb(all);
+						count--;
 					}
 					break ;
 				}
-				counter++;
-				all->b = all->b->next;
+				count++;
+				tmp_b = tmp_b->next;
 			}
 		}
 		tmp_a = tmp_a->next;
-		size--;
+		size_a--;
 	}
-	all
-	while ()
+	while (size_b)
+	{
+		ft_pa(all);
+		size_b--;
+	}
+	return (instructions);
+}
+
+char	*ft_sort_less(all, tmp_a, size_a, instructions)
+{
+	t_stack	*tmp_b;
+	int		count;
+	int		size_b;
+
+	size_b = 0;
+	while (size_a)
+	{
+		if (tmp_a->n >= all->mean)
+		{
+			ft_pb(all);
+			size_b++;
+			tmp_b = all->b;
+			count = 0;
+			while (count < size_b)
+			{
+				if (tmp_b->n < all->b)
+				{
+					while (count)
+					{
+						ft_sb(all);
+						ft_rb(all);
+						count--;
+					}
+					break ;
+				}
+				count++;
+				tmp_b = tmp_b->next;
+			}
+		}
+		tmp_a = tmp_a->next;
+		size_a--;
+	}
+	while (size_b)
+	{
+		ft_pa(all);
+		size_b--;
+	}
+	return (instructions)
+}
+
+char	*ft_sort(t_all *all, char *instructions)
+{
+	t_stack *tmp_a;
+	int		size_a;
+
+	tmp_a = all->a;
+	size_a = all->len;
+	instructions = ft_sort_less(all, tmp_a, size_a, instructions);
+	instructions = ft_sort_more(all, tmp_a, size_a, instructions);
 }
 
 void	ft_push_swap(char **av)
@@ -60,7 +116,7 @@ void	ft_push_swap(char **av)
 	instructions = NULL;
 	all = ft_initialize(all);
 	ft_fill_a(all, av);
-//	ft_find_min_max(all);
+	ft_find_ref(all);
 	instructions = ft_sort(all, instructions);
 	ft_putstr_fd(instructions, 0);
 }
