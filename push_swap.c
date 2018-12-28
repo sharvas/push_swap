d48762013c23848cab76ee99a9df9f8e5ab1a7ad
Sarunas VASKELIS <svaskeli@student.42.fr>
2018-12-28 14:31:17 +0100

    push-swap

--- push_swap.c
+++ push_swap.c
@@ -6,24 +6,35 @@
 /*   By: svaskeli <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/28 09:12:37 by svaskeli          #+#    #+#             */
-/*   Updated: 2018/12/28 13:45:59 by svaskeli         ###   ########.fr       */
+/*   Updated: 2018/12/28 14:28:57 by svaskeli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+char	*ft_sort(t_all *all, char *instructions)
+{
+	instruct
+
+}
+
 void	ft_push_swap(char **av)
 {
 	t_all	*all;
+	char	*instructions;
 
 	all = NULL;
+	instructions = NULL;
 	all = ft_initialize(all);
 	ft_fill_a(all, av);
-	all = ft_
+//	ft_find_min_max(all);
+	instructions = ft_sort(all, instructions);
+	ft_putstr_fd(instructions, 0);
 }
 
-int main(int ac, char **av)
+int		main(int ac, char **av)
 {
-
-
+	if (ac > 1)
+		ft_push_swap(av);
+	return (0);
 }
