cf9346f22aa4c809ad4d8f132d8b967d953dfffe
Sarunas <sarunas@Saruno-MacBook.local>
2019-01-01 10:43:07 +0100

    quicksort - need to fix with the swaping

--- quicksort.c
+++ quicksort.c
@@ -28,35 +28,31 @@ void	ft_print_list(t_all *all)
 void	swap(int *a, int *b, t_all *all)
 {
 //	int t;
-	t_stack *tmp;
-	t_stack *mem_a;
-	t_stack	*mem_b;
 	int		count_a;
 	int		count_b;
 	int		i;
+	int		first_a;
 
 	count_a = 0;
 	count_b = 0;
 	i = 0;
-	tmp = all->a;
-	while (tmp)
-	{
-		if (tmp->n == *a)
-			mem_a = tmp;
-		if (tmp->n == *b)
-			mem_b = tmp;
-		tmp = tmp->next;
-	}
-	while (i++ < all->len && all->a != mem_a)
+	first_a = 0;
+	while (i++ < all->len && (all->a->n != *a || all->a->n != *b))
 	{
 		ft_ra(all);
 		count_a++;
+		count_b = (all->a->n == *a) ? 0 : count_a;
+		count_a = (all->a->n == *a) ? count_a : 0;
+		first_a = (all->a->n == *a) ? 1 : 0;
 	}
 	ft_pb(all);
-	while (i++ < all->len - 1 && all->a != mem_b)
+	while (i++ < all->len - 1 && (all->a->n != *b || all->a->n != *a))
 	{
 		ft_ra(all);
 		count_b++;
+		count_a = (all->a->n == *b) ? 0 : count_b;
+		count_a = (all->a->n == *a) ? count_a : 0;
+		first_a = (all->a->n == *a) ? 1 : 0;
 	}
 	ft_pb(all);
 	ft_sb(all);
@@ -66,8 +62,6 @@ void	swap(int *a, int *b, t_all *all)
 	ft_pa(all);
 	while (count_a--)
 		ft_rra(all);
-//	ft_print_list(all);
-//	exit(1);
 //	t = *a;
 //	*a = *b;
 //	*b = t;
@@ -81,14 +75,13 @@ t_stack	*partition(t_stack *head, t_stack *last, t_all *all)
 	// similar to i = l-1 for array implementation 
 	t_stack *i;
 	t_stack	*j;
-//	t_stack *top;
 
 	i = head->prev;
 	j = head;
 	x = last->n;
-//	top = all->a;
 	// Similar to "for (int j = l; j <= h- 1; j++)"
-	while (j != last)
+	printf("	a\n");
+	while (j && j != last)
 	{
 		if (j->n <= x)
 		{
@@ -97,6 +90,7 @@ t_stack	*partition(t_stack *head, t_stack *last, t_all *all)
 			swap(&(i->n), &(j->n), all);
 		}
 		j = j->next;
+		printf("	b\n");
 	} 
 	i = (i == NULL) ? head : i->next; // Similar to i++
 	swap(&(i->n), &(last->n), all);
@@ -124,11 +118,11 @@ void	quickSort(t_all *all)
 	head = all->a;
 	tmp = all->a;
 	// Find last node 
-	while (tmp && tmp->next)
+	while (tmp->n != all->median)//(tmp && tmp->next)
 		tmp = tmp->next;
 	last = tmp;
 	// Call the recursive QuickSort 
-//	ft_print_list(all);
+	ft_print_list(all);
 	_quickSort(head, last, all);
-//	ft_print_list(all);
+	ft_print_list(all);
 } 
